b25a243e9c20ecda3fbee0ff3bcdffbe7ca20723
Dominik Stolzenburg <dominik.stolzenburg@tuwien.ac.at>
2024-03-19 06:31:09 +0100

    adjusted readthedocs.yaml to simplify dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ release = '0.1.0'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
 ]
 
-autodoc_mock_imports = ['numpy','scipy','pandas'
+autodoc_mock_imports = ['numpy'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
